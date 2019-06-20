552a6f81d8e0cfb484e1dd990d5d355a83a8ae67
splanard-thales <sebastien.planard@mythalesgroup.io>
2019-06-20 12:06:07 +0200

    Sonar: correction des tests sans assertion remontés par Monext (Gerald Levi)

--- src/test/java/com/payline/payment/slimpay/bean/common/SlimPayOrderItemTest.java
+++ src/test/java/com/payline/payment/slimpay/bean/common/SlimPayOrderItemTest.java
@@ -10,18 +10,18 @@ import org.powermock.reflect.Whitebox;
 
 import static com.payline.payment.slimpay.utils.BeansUtils.createDefaultMandate;
 import static com.payline.payment.slimpay.utils.BeansUtils.createDefaultPayin;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
 
 
 @PrepareForTest({SlimPayOrderItem.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SlimPayOrderItemTest {
 
-    SlimPayOrderItem orderItem;
     private Logger mockLogger;
 
     @BeforeEach
     public void setUp() {
-
         mockLogger = Mockito.mock(Logger.class);
 
         Whitebox.setInternalState(SlimPayOrderItem.class, "LOGGER", mockLogger);
@@ -29,37 +29,37 @@ public class SlimPayOrderItemTest {
 
     @Test
     public void SlimpayOrderItemOK(){
-        orderItem = SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
+        SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
                 .withType("payment")
                 .withPayin(createDefaultPayin("reference payment"))
                 .build();
 
-
+        Mockito.verify(mockLogger, never()).warn(anyString());
     }
 
     @Test
     public void SlimpayOrderItemMandateOK(){
-        orderItem = SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
+        SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
                 .withType("signMandate")
                 .withMandate(createDefaultMandate("reference mandate"))
                 .build();
 
+        Mockito.verify(mockLogger, never()).warn(anyString());
     }
 
     @Test
     public void SlimpayOrderItemMandateWithoutReference(){
-        orderItem = SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
+        SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
                 .withType("signMandate")
                 .build();
-        Mockito.verify(mockLogger, Mockito.times(1)).warn(Mockito.eq(orderItem.MANDATE_WARN));
 
+        Mockito.verify(mockLogger, Mockito.times(1)).warn(Mockito.eq(SlimPayOrderItem.MANDATE_WARN));
     }
     @Test
     public void SlimpayOrderItemMandateWithoutType(){
-        orderItem = SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
+        SlimPayOrderItem.Builder.aSlimPayOrderItemBuilder()
                 .build();
-        Mockito.verify(mockLogger, Mockito.times(1)).warn(Mockito.eq(orderItem.TYPE_WARN));
-        Mockito.verify(mockLogger, Mockito.times(1)).warn(Mockito.eq(orderItem.TYPE_WARN));
 
+        Mockito.verify(mockLogger, Mockito.times(1)).warn(Mockito.eq(SlimPayOrderItem.TYPE_WARN));
     }
 }
